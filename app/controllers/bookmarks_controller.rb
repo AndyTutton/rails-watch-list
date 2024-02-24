285021d29ef1fefd7af0f66d3e34a0c5b4c3ad11
Andy Tutton <andytutton2@gmail.com>
2024-02-24 13:10:31 +0000

    working through bookmarks action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,26 @@
 class BookmarksController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
-  end
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
 
-  def edit
-  end
-
-  def update
+    @bookmark.save
+    redirect_to list_path(@list)
   end
 
   def destroy
   end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,8 +12,17 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
   end
 
   def destroy
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
