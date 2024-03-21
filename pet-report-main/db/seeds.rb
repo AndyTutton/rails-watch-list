3025b9a5d296d3230770822a893236aed06479b7
Andy Tutton <andytutton2@gmail.com>
2024-03-21 15:05:25 +0000

    testing

---
+++ pet-report-main/db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Pet.create(name: 'Leonardo', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Raphael', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Donatello', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Michaelangelo', address: "Sewer lair", species: "turtle")
+Pet.create(name: 'Cleo', address: "Basecamp, species: "dog")
+Pet.create(name: 'Riley', address: "HQ", species: "dog")
+Pet.create(name: 'Garfield', address: "Bromley", species: "cat")
+
+
+
+
+
+
+
+
